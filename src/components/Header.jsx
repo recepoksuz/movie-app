6ded27907077fb4d7a2196031cf77d680f50ec9e
Recep Öksüz <recepoksuziletisim@gmail.com>
2023-05-30 11:13:54 +0300

    logo-problems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,12 +11,25 @@ import { useRouter } from 'next/navigation'
 const Header = () => {
   const [keyword, setKeyword] = useState("")
   const router = useRouter()
+  const [logo, setLogo] = useState("https://i.hizliresim.com/8p35qo8.png")
+
+  let {systemTheme, theme, setTheme} = useTheme()
+  const themeMode = theme === "system" ? systemTheme : theme
 
   const handleKeyUp = (event)=>{
     if(event.key === "Enter"){
       router.push(`/search/${event.target.value}`)
     }
   }
+
+  useEffect(() => {
+    console.log(localStorage.getItem("theme"))
+    if(localStorage.getItem("theme") === "light"){
+      setLogo("https://i.hizliresim.com/8p35qo8.png")
+    }else {
+      setLogo("https://i.hizliresim.com/dqbcaex.png")
+    }
+  }, [theme]);
 
   const menu = [
     {
@@ -31,7 +44,7 @@ const Header = () => {
   
   return (
     <div className='flex justify-center items-center gap-6 h-9 p-8 '>
-      <img onClick={() => router.push("/")} src={`${localStorage.getItem("theme") === "light" ? "https://i.hizliresim.com/8p35qo8.png" : "https://i.hizliresim.com/dqbcaex.png"}`} className='h-9 hover:cursor-pointer' />
+      <img onClick={() => router.push("/")} src={`${logo}`} className='h-9 hover:cursor-pointer' />
       {/* console.log(localStorage.getItem("theme")) */}
       <div className='flex flex-1 justify-center gap-3 items-center border border-slate-500 rounded-lg p-2'>
         <input type="text" onKeyUp={handleKeyUp} onChange={(e)=>setKeyword(e.target.value)} placeholder='Film Arayın' className='flex-1 h-6 p-2 outline-none bg-transparent' />
