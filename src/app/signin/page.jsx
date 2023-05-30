73166202df697ff5d0d4d7e4c28cba1a6a621d31
Recep Öksüz <recepoksuziletisim@gmail.com>
2023-05-30 12:39:42 +0300

    about and login add

---
+++ src/app/signin/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='flex justify-center font-bold text-3xl'>
+    {"Yakında... Demek İsterdim:)"}
+</div>
+  )
+}
+
+export default Page
